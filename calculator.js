1e0c9532838743e68b9d4e584ebe2c3b6c0c425f
Mahmood28 <mahmoodalwatani@gmail.com>
2021-01-10 16:37:15 +0300

    Solved the Conditionals task.

--- calculator.js
+++ calculator.js
@@ -1,23 +1,14 @@
-const num1 = prompt("Please enter the first integer")
-const op = prompt("Please enter the operation")
-const num2 = prompt("Please enter the second integer")
+const num1 = parseInt(prompt('Please enter the first integer'));
+var op = prompt('Please enter the operation');
+const num2 = parseInt(prompt('Please enter the second integer'));
 
-let x = null;
+while(!(op==='*'||op==='-'||op==='+'||op==='/'))  
+{
+op = prompt('Invalid opeation. Please input a valid operation to proceed.')
+}
 
-while(!(op==='*'||op==='-'||op==='+'||op==='/')) {
-If(op==='*')
-{
-  x = num1*num2 
-} 
-elseif (op==='+'){
-  x = num1+num2
-} elseif(op==='-')
-{
-  x = num1-num2
-} elseif(op==='/') {
-  x = num1/num2
-} else {
-  op = prompt("Invalid opeation. Please input a valid operation to proceed.")
-}
-}
-console.log('${num1}${op}${num2} = ${x} )
+if(op==='*') console.log(`${num1}${op}${num2} = ${num1*num2}`);
+else if (op==='+') console.log(`${num1}${op}${num2} = ${num1+num2}`);
+else if(op==='-') console.log(`${num1}${op}${num2} = ${num1 - num2}`);
+else  console.log(`${num1}${op}${num2} = ${num1/num2}`);
+
